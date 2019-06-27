838f86d430fa09ffe9a2f4d45bea8e1b1a624373
SRUJANA <SRUJANA@DESKTOP-2Q9LFBI>
2019-06-27 16:23:35 +0530

    2nd commit

---
+++ src/main/java/com/srujana/collabapi/controller/BlogController.java
@@ -0,0 +1,38 @@
+package com.srujana.collabapi.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.srujana.collabapi.model.Blog;
+
+@RestController
+public class BlogController {
+
+	public BlogController()
+	{
+		System.out.println("Blog Controller");
+	}
+	@RequestMapping("/getBlog")
+	public List<Blog> getAllBlogs()
+	{
+		Blog b1=new Blog();
+		Blog b2=new Blog();
+		b1.setBlogName("flipcart");
+		b1.setBlogTitle("flipcart");
+		b1.setBlogOwner("filpcart");
+		
+		b2.setBlogName("Amazon");
+		b2.setBlogTitle("Amazon");
+		b2.setBlogOwner("Amazon");
+		
+		
+		List<Blog> bList=new ArrayList<Blog>();
+		bList.add(b1);
+		bList.add(b2);
+		return bList;
+		
+	}
+}
